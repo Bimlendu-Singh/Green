ef7d6b22a0db902d177232c650e9a453882d3c83
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-10-12 10:14:38 +0530

    updated

--- program2.cpp
+++ program2.cpp
@@ -37,3 +37,57 @@ void search(int x);
 void count();
 void delfirst();
 void dellast();
+
+//Function for delete from last
+ void dellast()
+ {
+   if(start==NULL)
+     printf("\nNo list is present\n");
+
+   else if(start->link==start)
+   {
+     free(start);
+     start = NULL;
+     printf("\nNode is deleted\n");
+   }
+
+   else
+   {
+     p=start;
+
+     while(p->link->link!=start)
+	    p=p->link;
+
+     temp = p->link;
+     free(temp);
+     p->link = start;
+     printf("\nNode is deleted\n");
+   }
+ }
+
+ //Function to delete from first
+ void delfirst()
+ {
+    if(start==NULL)
+     printf("\nNo list is present\n");
+
+    else if(start->link==start)
+    {
+      free(start);
+      start=NULL;
+      printf("\nNode is deleted\n");
+    }
+
+    else
+    {
+      p = start;
+      while(p->link!=start)
+	    p=p->link;
+
+      temp = start;
+      start = start->link;
+      p->link = start;
+      free(temp);
+      printf("\nNode is deleted\n");
+    }
+ }
