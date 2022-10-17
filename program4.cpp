752c3e5c06bb26db9d5ab8432e3045fccf26b635
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-10-17 11:05:58 +0530

    updated

--- program4.cpp
+++ program4.cpp
@@ -18,3 +18,23 @@ void pop();
 void search(int x);
 void count();
 void display();
+
+//Main
+void main()
+{
+  int ch,n;
+  front = NULL;   //Initialising front and rear to NULL
+  rear = NULL;
+  clrscr();
+
+  do
+  {
+    clrscr();
+    //Choices
+    printf("\n\n\t\tEnter your choice: ");
+    printf("\n\n\t\tPress 1 to insert ");
+    printf("\n\n\t\tPress 2 to delete");
+    printf("\n\n\t\tPress 3 to search");
+    printf("\n\n\t\tPress 4 to count");
+    printf("\n\n\t\tPress 5 to display\n\n");
+    scanf("%d",&ch);
