971d3e8c46a7911108dcb7a2c895c458e165f269
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-10-17 11:07:32 +0530

    updated

--- program4.cpp
+++ program4.cpp
@@ -38,3 +38,149 @@ void main()
     printf("\n\n\t\tPress 4 to count");
     printf("\n\n\t\tPress 5 to display\n\n");
     scanf("%d",&ch);
+
+    switch(ch)
+    {
+      //Insertion Choice
+      case 1: printf("Enter the value to insert\n");
+	      scanf("%d",&n);
+	      insert(n);
+	      break;
+
+      //Deletion
+      case 2: pop();
+	      break;
+
+      //Element search operation
+      case 3: printf("Enter the vlaue to be search\n");
+	      scanf("%d",&n);
+	      search(n);
+	      break;
+
+      //Counting of elements in queue
+      case 4: count();
+	      break;
+
+      //Display of all elements in queue
+      case 5: display();
+	      break;
+
+      default: printf("\nInvalid choice");
+    }
+    printf("\nPress 0 to continue...");
+    scanf("%d",&ch);
+
+  }while(ch==0);
+}
+ //Insert Function
+ void insert(int x)
+ {
+    if(rear==NULL)
+    {
+      rear = (struct Node*)malloc(sizeof(struct Node));
+      rear->data = x;
+      rear->link = NULL;
+      front = rear;
+    }
+    else
+    {
+      temp = (struct Node*)malloc(sizeof(struct Node));
+      temp->data = x;
+      temp->link = NULL;
+      rear->link = temp;
+      rear = temp;
+    }
+    printf("Element inserted...");
+ }
+ //Delete function
+ void pop()
+ {
+   if(front==NULL)
+     printf("No list is present!!!");
+
+   else if(front==rear&&rear!=NULL)    //when front and rear both pointing
+   {                                   //same node and we have to delete
+     temp = front;                     //that node
+     free(temp);
+     front=NULL;
+     rear=NULL;
+     printf("\nNode is delted...\n");
+   }
+   else
+   {
+     temp = front;
+     front = front->link;
+     free(temp);
+     printf("\nNode is delted...\n");
+   }
+ }
+ //Search function
+ void search(int x)
+ {
+   int flag = 0 ;
+
+   if(front==NULL)
+    {
+      printf("No list is present!!!");
+    }
+   else
+   {
+     p = front;
+
+     while(p!=NULL)
+     {
+       if(p->data == x)
+       {
+
+	       flag++;
+	       printf("%dtest",flag);
+       }
+
+       p=p->link;
+     }
+   }
+
+    if(flag)
+     printf("\nElements found %d number of times", flag);
+    else
+     printf("\nElement not found\n");
+ }
+
+    //Count function
+ void count()
+ {
+  int c = 0;
+
+  if(front==NULL)
+    printf("No list present");
+
+  else
+  {
+    p = front;
+
+    while(p!=NULL)
+    {
+      c++;
+      p = p->link;
+    }
+    printf("%d elements are in queue", c);
+  }
+ }
+ //Display function
+ void display()
+ {
+   if(front==NULL)
+    printf("No list present");
+
+   else
+   {
+     p = front;
+
+     printf("Elements are : ");
+     while(p!=NULL)
+     {
+       printf(" %d\t",p->data);
+       p = p->link;
+     }
+   }
+ }
