fe6f928bbc5570c6e7ae2f4ad95fd196cc8c3f1f
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-10-20 18:54:24 +0530

    updated

--- program2.cpp
+++ program2.cpp
@@ -37,3 +37,37 @@ void main()
     printf("Press 6 to dellast\n");
 
     scanf("%d",&ch);
+
+    switch(ch)
+    {
+      //Insertion Choice
+      case 1: printf("Enter the value to insert\n");
+	      scanf("%d",&n);
+	      insert(n);
+	      break;
+
+      //Deletion from first
+      case 2: display();
+	      break;
+
+      case 3: printf("Enter the value to search in the list\n");
+	      scanf("%d",&n);
+	      search(n);
+	      break;
+
+      case 4: count();
+	      break;
+
+      case 5: delfirst();
+	      break;
+
+      case 6: dellast();
+	      break;
+
+      default: printf("\nInvalid choice");
+    }
+    printf("\nPress 0 to continue...");
+    scanf("%d",&ch);
+
+  }while(ch==0);
+}
