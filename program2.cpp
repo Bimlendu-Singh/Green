775e403c8533d5de3f9eff2f08b2816d27c23f1c
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-10-20 18:53:55 +0530

    updated

--- program2.cpp
+++ program2.cpp
@@ -17,3 +17,23 @@ void search(int x);
 void count();
 void delfirst();
 void dellast();
+
+//Main
+void main()
+{
+  int ch,n;
+  clrscr();
+
+  do
+  {
+    clrscr();
+    //Choices
+    printf("\n\nEnter your choice: \n");
+    printf("\nPress 1 to insert \n");
+    printf("Press 2 to display\n");
+    printf("Press 3 to search\n");
+    printf("Press 4 to count\n");
+    printf("Press 5 to delfirst\n");
+    printf("Press 6 to dellast\n");
+
+    scanf("%d",&ch);
