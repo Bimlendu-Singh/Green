472dd46da0c3506f3982f6d49bc02c2539cf783b
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-10-24 18:04:19 +0530

    updated

--- program3.cpp
+++ program3.cpp
@@ -18,3 +18,21 @@ void pop();
 void search(int x);
 void count();
 void display();
+
+void main()
+{
+  int ch,n;
+  start = NULL;
+  clrscr();
+
+  //Menu driven
+  do
+  {
+   clrscr();
+   printf("\n\n\t\tEnter your choice :");
+   printf("\n\n\t\tPress 1 to insert ");
+   printf("\n\n\t\tPress 2 to delete");
+   printf("\n\n\t\tPress 3 to search");
+   printf("\n\n\t\tPress 4 to count");
+   printf("\n\n\t\tPress 5 to display");
+   scanf("%d",&ch);
