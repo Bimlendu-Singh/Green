4946fc3bbfbabf022974bb7a8cbaa960f5043457
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-10-26 15:22:06 +0530

    updated

---
+++ program5.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+	      
+void main()                                       
+{
+ int arr[10],i,j,flag=0,mid,lower=0,upper=9,x,temp;  //Declaring and initializing variables
+ clrscr();   
+
+ //Input                              
+ cout<<"Enter 10 elements in the array\n ";        
+  for(i=0;i<10;i++)
+   cin>>arr[i];
