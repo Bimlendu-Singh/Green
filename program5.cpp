aeb9975323a86089cfe057bf6e9a04a68529eec2
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-10-31 13:47:14 +0530

    updated

--- program5.cpp
+++ program5.cpp
@@ -18,3 +18,72 @@ void pop();
 void search(int x);
 void count();
 void display();
+
+void main()
+{
+  int ch,n;
+  start = NULL;
+  clrscr();
+
+  //Menu driven
+  do
+  {
+   clrscr();
+   printf("\n\n\t\tEnter your choice :");
+   printf("\n\n\t\tPress 1 to insert ");
+   printf("\n\n\t\tPress 2 to delete");
+   printf("\n\n\t\tPress 3 to search");
+   printf("\n\n\t\tPress 4 to count");
+   printf("\n\n\t\tPress 5 to display");
+   scanf("%d",&ch);
+
+   switch(ch)
+   {
+     case 1: printf("\n Enter the value to insert");
+	           scanf("%d",&n);
+	           insert(n);
+	           break;
+
+     case 2: pop();
+	     break;
+
+     case 3: printf("Enter value to be search ");
+	     scanf("%d",&n);
+	     search(n);
+	     break;
+
+     case 4: count();
+	     break;
+
+     case 5: display();
+	     break;
+
+     default: printf("\nInvalid choice");
+
+   }
+   printf("\nPress 0 to continue...");
+   scanf("%d",&ch);
+
+  }while(ch==0);
+}
+ //Insert operation
+ void insert(int x)
+ {
+   if(start==NULL)
+   {
+    start=(struct Node*)malloc(sizeof(struct Node));
+    start->link=NULL;
+    start->data=x;
+   }
+   else
+   {
+    p=start;
+    while(p->link!=NULL)
+	  p=p->link;
+
+    temp = (struct Node*)malloc(sizeof(struct Node));
+    p->link=temp;
+    temp->link=NULL;
+    temp->data=x;
+   }
+ }
