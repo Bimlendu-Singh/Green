7d76f3bcbeb4efa80a96d9dff8f629c87202a01f
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-11-08 12:06:53 +0530

    updated

---
+++ program1.cpp
@@ -0,0 +1,19 @@
+//Circular Linked List
+
+#include<stdio.h>
+#include<conio.h>
+#include<alloc.h>
+
+struct Node                                   
+{
+  int data;
+  struct Node *link;
+}*p,*start,*temp;
+
+//Declaring function
+void insert(int x);
+void display();
+void search(int x);
+void count();
+void delfirst();
+void dellast()
