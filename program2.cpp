7ad2adf8780193b7014439dc70a407a4d5496b51
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-11-09 10:22:53 +0530

    updated

--- program2.cpp
+++ program2.cpp
@@ -17,3 +17,57 @@ void search(int x);
 void count();
 void delfirst();
 void dellast();
+
+//Main
+void main()
+{
+  int ch,n;
+  clrscr();
+
+  do
+  {
+    clrscr();
+    //Choices
+    printf("\n\nEnter your choice: \n");
+    printf("\nPress 1 to insert \n");
+    printf("Press 2 to display\n");
+    printf("Press 3 to search\n");
+    printf("Press 4 to count\n");
+    printf("Press 5 to delfirst\n");
+    printf("Press 6 to dellast\n");
+
+    scanf("%d",&ch);
+
+    switch(ch)
+    {
+      //Insertion Choice
+      case 1: printf("Enter the value to insert\n");
+	      scanf("%d",&n);
+	      insert(n);
+	      break;
+
+      //Deletion from first
+      case 2: display();
+	      break;
+
+      case 3: printf("Enter the value to search in the list\n");
+	      scanf("%d",&n);
+	      search(n);
+	      break;
+
+      case 4: count();
+	      break;
+
+      case 5: delfirst();
+	      break;
+
+      case 6: dellast();
+	      break;
+
+      default: printf("\nInvalid choice");
+    }
+    printf("\nPress 0 to continue...");
+    scanf("%d",&ch);
+
+  }while(ch==0);
+}
