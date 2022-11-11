01d34722590544189946e49f17d97ee151649713
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-11-11 11:08:37 +0530

    updated

--- program1.cpp
+++ program1.cpp
@@ -18,3 +18,24 @@ void pop();
 void search(int x);
 void count();
 void display();
+
+//Queue by Linked List implementation
+
+#include<stdio.h>
+#include<conio.h>
+#include<alloc.h>
+
+//Node structure
+struct Node                               
+{
+  int data;
+  struct Node* link;
+
+}*front,*rear,*p,*temp;
+
+//Function Declaration
+void insert(int x);
+void pop();
+void search(int x);
+void count();
+void display();
