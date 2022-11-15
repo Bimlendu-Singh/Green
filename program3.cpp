1524739d06b9f27fd9deff43257e0e335038f0bc
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-11-15 09:06:42 +0530

    updated

--- program3.cpp
+++ program3.cpp
@@ -27,3 +27,32 @@ void main()
   for(i=0;i<3;i++)
    for(j=0;j<3;j++)
     scanf("%d",&Y[i][j]);
+
+    //Displaying matrix Y
+ printf("\n Matrix Y is \n");    
+  for(i=0;i<3;i++)
+   {
+    printf("\n\n");
+     for(j=0;j<3;j++)
+      printf(" %d ", Y[i][j]);
+   }
+
+ //Loop and logic for multiplying two matrix
+ for(i=0;i<3;i++)          
+  for(j=0;j<3;j++)
+   {
+    Z[i][j]=0;
+     for(k=0;k<3;k++)
+      Z[i][j]=X[i][k]*Y[k][j]+Z[i][j];
+   }
+
+ //Displaying finally matrix Z
+ printf("\n\n After multiplication Matrix Z is \n");
+  for(i=0;i<3;i++)
+   {                            
+    printf("\n\n");
+     for(j=0;j<3;j++)
+      printf(" %d ", Z[i][j]);
+   }
+   getch();
+}
