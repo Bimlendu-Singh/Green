42bd6f383818cdb4d5e5effb8dc254f5d887f775
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-12-06 09:36:15 +0530

    updated

---
+++ program2.cpp
@@ -0,0 +1,24 @@
+//10. Write a program to print all Armstrong numbers under 1000
+
+#include<stdio.h>
+
+int main()
+{
+   int rem,sum=0,count=0,res,i,j,k,x;
+
+    printf("\n Armstrong number under 1000 are : ");
+
+   //Loop to find armstrong numbe under 1000
+   for(i=1;i<=1000;i++)
+   {
+     x = i;
+     count = 0;
+     sum = 0;
+
+    //Loop counting number of digits in the given number
+     while(x!=0)
+     {
+      count++;
+      x = x/10;
+     }
+     x = i;  //restoring original value of x
