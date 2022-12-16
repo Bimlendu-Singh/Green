663e4b9a59da211bf8a36692bb9ad36989a88473
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-12-16 21:55:44 +0530

    updated

--- program2.cpp
+++ program2.cpp
@@ -21,3 +21,26 @@ int main()
       count++;
       x = x/10;
      }
+
+     x = i;  //restoring original value of x
+
+    //Loop to check whether the 'i' is armstrong number or not.
+     for(j=1;j<=count;j++)
+     {
+      res = 1;
+      rem = x%10;
+      for(k=1;k<=count;k++)
+       res = res*rem;
+
+      sum = sum + res;
+      x = x/10;
+     }
+    
+     //Printing armstrong number only
+     if(sum==i)
+      printf(" %d ",sum);
+   } 
+
+   return 0;
+
+}
