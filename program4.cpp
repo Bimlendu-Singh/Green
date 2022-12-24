c07387e8200055fc77545aee7932fe18998d259a
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-12-24 14:26:47 +0530

    updated

--- program4.cpp
+++ program4.cpp
@@ -8,3 +8,20 @@ int main()
 
     printf("\nEnter number to check prime or not : ");
     scanf("%d",&x);
+
+       for(i=2;i<=x/2;i++)
+    {
+        if(x%i==0)
+        {
+          count++;
+          break;
+        }
+    }
+
+    if(count)
+     printf("\nNumber is not prime ");
+     else
+      printf("\nNumber is prime ");    
+
+    return 0;
+}
