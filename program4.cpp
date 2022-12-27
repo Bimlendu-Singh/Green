24785af1805eeb567d689cdb72f4b8d090b9d945
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-12-27 10:05:27 +0530

    updated

--- program4.cpp
+++ program4.cpp
@@ -1,3 +1,10 @@
 //6. Write a program to calculate factorial of a number
 
 #include<stdio.h>
+
+int main()
+{
+    int x,res=1,i;
+
+    printf("\nEnter number to find its factorial : ");
+    scanf("%d",&x);
