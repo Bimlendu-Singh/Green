d7d5a4173ce6b938b9f9fece332c14f9797df5d5
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2023-01-11 21:39:44 +0530

    updated

--- program2.cpp
+++ program2.cpp
@@ -8,3 +8,15 @@ int main()
 
     printf("\nEnter number to find its factorial : ");
     scanf("%d",&x);
+
+    
+        for(i=1;i<=x;i++)
+        {
+         res = res*i;
+        }
+
+     printf("\nFactorial is : %d",res);
+
+ 
+    return 0;
+}
