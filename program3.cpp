6e437ada2160201483fe984e555e89aef04eff04
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2023-01-18 22:15:15 +0530

    updated

--- program3.cpp
+++ program3.cpp
@@ -5,3 +5,15 @@
 int main()
 {
     int x,i,count=0;
+
+        printf("\nEnter number to check prime or not : ");
+    scanf("%d",&x);
+
+    for(i=2;i<=x/2;i++)
+    {
+        if(x%i==0)
+        {
+          count++;
+          break;
+        }
+    }
