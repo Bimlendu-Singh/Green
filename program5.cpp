0c73a9752268d17c44b27cacfd3205d29551b4f1
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2023-01-21 22:05:05 +0530

    updated

--- program5.cpp
+++ program5.cpp
@@ -11,3 +11,15 @@ int main()
    //Position input 
    printf("\nEnter the position : ");
    scanf("%d",&pos);
+
+    else if(pos>3)
+    {
+      res = 0;  
+
+     for(i=1;i<pos-1;i++)
+     {
+       res = new + prev;    //Here, 'new' and 'prev' variable to store last and 2nd last digit so we can add them and store result in 'res' variable
+       prev = new;
+       new = res; 
+     }
+    }s
