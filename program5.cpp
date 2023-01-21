bf7a5ee0b7c9b0f63b761cbb81254741e0f3718e
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2023-01-21 22:04:47 +0530

    updated

--- program5.cpp
+++ program5.cpp
@@ -1,3 +1,13 @@
 //Assignment7Qn1.c
 
 //1. Write a program to find the Nth term of the Fibonnaci series.
+
+#include<stdio.h>
+
+int main()
+{
+   int res = 1,pos,i,prev=0,new=1;
+
+   //Position input 
+   printf("\nEnter the position : ");
+   scanf("%d",&pos);
