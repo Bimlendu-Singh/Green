548108f1c6cf16f77f8d37f36b1793f6784448f1
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2023-01-25 09:43:16 +0530

    updated

--- program2.cpp
+++ program2.cpp
@@ -1,3 +1,10 @@
 //8. Write a program to check whether a given number is a Prime number or not
 
 #include<stdio.h>
+
+int main()
+{
+    int x,i,count=0;
+
+    printf("\nEnter number to check prime or not : ");
+    scanf("%d",&x);
