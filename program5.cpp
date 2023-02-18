19f96913044a9c2aa971bf70f3603ad82f109131
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2023-02-18 17:54:14 +0530

    updated

--- program5.cpp
+++ program5.cpp
@@ -10,3 +10,23 @@ int main()
     printf("\nEnter number : ");
     scanf("%d",&n);
     y=n;  //Storing the value of 'n' to 'y' so we don't loose the original number
+
+//Loop to count the digit of the given number
+    for(i=n;i!=0;i/=10)
+     count++;
+
+    //Loop to reverse the entered number
+    for(i=1;i<=count;i++)
+    {
+        x = n%10;
+        num =  num*10 + x;
+        n/=10;
+    }
+
+    //Display
+    printf("\nReverse of %d is %d\n", y,num);
+
+
+ 
+    return 0;
+}    
