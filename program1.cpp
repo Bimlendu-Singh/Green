83e990ae0155d14565e8c6767e631ad1a556f9e7
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2023-04-02 09:16:39 +0530

    updated

--- program1.cpp
+++ program1.cpp
@@ -7,3 +7,10 @@ int main()
    int rem,sum=0,count=0,res,i,j,k,x;
 
     printf("\n Armstrong number under 1000 are : ");
+
+//Loop to find armstrong numbe under 1000
+   for(i=1;i<=1000;i++)
+   {
+     x = i;
+     count = 0;
+     sum = 0;
