23b7db262c82c8b4e7d1de0c5f60bc2e496d6083
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2023-05-05 09:48:45 +0530

    updated

--- program6.c
+++ program6.c
@@ -1,3 +1,16 @@
 //10. Write a program to reverse a given number
 
 #include<stdio.h>
+
+int main()
+{
+    int n,i,x,y,count = 0,num=0;
+
+    //Input
+    printf("\nEnter number : ");
+    scanf("%d",&n);
+    y=n;  //Storing the value of 'n' to 'y' so we don't loose the original number
+   
+    //Loop to count the digit of the given number
+    for(i=n;i!=0;i/=10)
+     count++;
