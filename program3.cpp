a1f58c6a02df382bde4d11a1b155b6789e8f0fc1
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2023-05-19 15:29:17 +0530

    update

--- program3.cpp
+++ program3.cpp
@@ -1,3 +1,10 @@
 //8. Write a program to find next Prime number of a given number
 
 #include<stdio.h>
+
+int main()
+{
+   int i,j,count=0,x;
+
+   printf("\nEnter a number to find next prime number from that number : ");
+   scanf("%d",&x);
