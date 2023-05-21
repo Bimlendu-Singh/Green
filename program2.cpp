aa808545f701ebb3f932ecf63d15443ddeb7cf5f
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2023-05-21 11:30:21 +0530

    update

--- program2.cpp
+++ program2.cpp
@@ -5,3 +5,9 @@
 int main()
 {
    int res=-1,pos,i,prev=0,new=1,x,j,flag=0;
+
+   //Fibonacci series 0,1,1,2,3,5,8,13,21,34,55,89,144...
+
+   //Number input 
+   printf("\nEnter the number to check whether it is in fibonacci series or not : ");
+   scanf("%d",&x);
