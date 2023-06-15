14c136b67ed0f4fef76a09be70421dc4b297fae3
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2023-06-15 11:53:46 +0530

    updated

--- phpprogram3.php
+++ phpprogram3.php
@@ -8,3 +8,12 @@ $rem=$x%10;
 $total=$total+$rem*$rem*$rem;  
 $x=$x/10;  
 }  
+if($num==$total)  
+{  
+echo "Yes it is an Armstrong number";  
+}  
+else  
+{  
+echo "No it is not an armstrong number";  
+}  
+?>  
