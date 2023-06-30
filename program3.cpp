8eb42b735a46b8fdb40b56dea5629342bb0c99f3
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2023-06-30 19:18:37 +0530

    updated

--- program3.cpp
+++ program3.cpp
@@ -5,3 +5,9 @@
 int main()
 {
    int res,pos,i,prev=0,new=1,n,j;
+
+   //Fibonacci series 0,1,1,2,3,5,8,13,21,34,55,89,144...
+
+   //Position input 
+   printf("\nEnter the nth term : ");
+   scanf("%d",&n);
