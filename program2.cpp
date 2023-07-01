4b2792bbc3e240ad4d5ecfcb37fde26434f7ae5b
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2023-07-01 23:09:48 +0530

    updated

--- program2.cpp
+++ program2.cpp
@@ -1,3 +1,9 @@
 //3. Write a program to check whether a given number is there in the Fibonacci series or not.
 
 #include<stdio.h>
+
+int main()
+{
+   int res=-1,pos,i,prev=0,new=1,x,j,flag=0;
+
+   //Fibonacci series 0,1,1,2,3,5,8,13,21,34,55,89,144...
