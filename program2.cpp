0f15d3a147c7fd9887bd88e2c72f306ae1eee03b
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2023-07-22 09:30:27 +0530

    updated

--- program2.cpp
+++ program2.cpp
@@ -14,3 +14,19 @@ int main()
    //Loop to count the digit of the given number
     for(i=n;i!=0;i/=10)
      count++;
+
+     //Loop to reverse the entered number
+    for(i=1;i<=count;i++)
+    {
+        x = n%10;
+        num =  num*10 + x;
+        n/=10;
+    }
+
+    //Display
+    printf("\nReverse of %d is %d\n", y,num);
+
+
+ 
+    return 0;
+}
