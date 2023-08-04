d35316b7255c44c9d2f2dcd56b4bda78bda9a596
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2023-08-04 10:00:39 +0530

    updated

--- program5.cpp
+++ program5.cpp
@@ -10,3 +10,14 @@ int main()
 
    //Loop to find armstrong numbe under 1000
    for(i=1;i<=1000;i++)
+   {
+     x = i;
+     count = 0;
+     sum = 0;
+
+    //Loop counting number of digits in the given number
+     while(x!=0)
+     {
+      count++;
+      x = x/10;
+     }
